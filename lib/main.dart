e780cde792157d500f7abd30d8d8833f636c38a2
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-15 17:48:31 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = Color(0xff141F47);
 Color cardColor = Color(0xff1C2D6B);
@@ -13,7 +15,7 @@ Color buttonColor = Color(0xff152F8D);
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: false,
       builder: (context) => MyApp(),
     ),
   );
@@ -38,14 +40,80 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Column(
-        children: [
-          // Input Card
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Input Card
 
-          // Output Card
+              // Output Card
 
-          // Select Language
-        ],
+              // Select Language
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: languageCardColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Indonesia",
+                          style: GoogleFonts.barlow(
+                            color: textColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "English",
+                          style: GoogleFonts.barlow(
+                            color: textColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 8),
+                          color: Colors.black.withValues(alpha: 0.3),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: CupertinoButton(
+                      color: buttonColor,
+                      padding: EdgeInsets.all(20),
+                      borderRadius: BorderRadius.circular(100),
+                      onPressed: () {},
+                      child: Image.asset(
+                        "assets/reverse.png",
+                        height: 25,
+                        width: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
